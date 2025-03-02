df0acdf9abb7e665baf27624c0579d1395ad6acd
OlegEgoism <olegpustovalov220@gmail.com>
2025-03-02 22:19:08 +0300

    Права доступа

---
+++ db_crp/templatetags/custom_filters.py
@@ -0,0 +1,20 @@
+# from django.template.defaultfilters import register
+#
+#
+# @register.filter
+# def get_item(dictionary, key):
+#     """Возвращает значение по ключу из словаря"""
+#     return dictionary.get(key, None)
+
+
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    """Возвращает значение по ключу из словаря"""
+    if isinstance(dictionary, dict):
+        return dictionary.get(key, None)
+    return None
